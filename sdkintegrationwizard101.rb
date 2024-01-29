df9fd8e30a1116bd5de68cefb600e048bea393cf
manoj.krishnan <manoj.krishnan@inmobi.com>
2024-01-29 16:17:36 +0530

    new formula tap created

---
+++ sdkintegrationwizard101.rb
@@ -0,0 +1,20 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Sdkintegrationwizard101 < Formula
+  desc ""
+  homepage "https://github.com/manoj897"
+  url "https://github.com/manoj897/android-sdk-integration-wizard/archive/refs/tags/v1.0.1.tar.gz"
+  sha256 "246b1e61652083f9543e988940a8ea6524f6746fdb315cdfe55e46afd2e1ecdf"
+  license ""
+
+  depends_on "openjdk@11"
+
+  def install
+        libexec.install Dir['*']
+        bin.write_jar_script libexec/"android-sdk-inmobisdkwizard.jar" "android-sdk-inmobisdkwizard" java_version: "11"
+  end
+
+  test do
+  end
+end
