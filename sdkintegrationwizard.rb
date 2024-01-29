1d31c3f4a564f96b5e73d29ca2037585a0e300c9
manoj.krishnan <manoj.krishnan@inmobi.com>
2024-01-29 12:08:13 +0530

    Add wizard changes as per homebrew created

---
+++ sdkintegrationwizard.rb
@@ -0,0 +1,21 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Sdkintegrationwizard < Formula
+  desc "SDK integration Wizard"
+  homepage "https://github.com/manoj897/android-sdk-integration-wizard"
+  url "https://github.com/manoj897/android-sdk-integration-wizard/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "81978ddd587b0d7a9c45f260451e8f73bfe9e578d3dd3020d82b4c96967d5d8f"
+  license ""
+
+  # depends_on "cmake" => :build
+  depends_on "openjdk@11"
+
+  def install
+    libexec.install Dir['*']
+    bin.write_jar_script libexec/"android-sdk-inmobisdkwizard.jar" "android-sdk-inmobisdkwizard" java_version: "11"
+  end
+
+  test do
+  end
+end
