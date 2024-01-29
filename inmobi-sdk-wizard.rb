60bc6972fc886baddf644d30423ec485658625fa
manoj.krishnan <manoj.krishnan@inmobi.com>
2024-01-29 11:23:55 +0530

    changed url

--- inmobi-sdk-wizard.rb
+++ inmobi-sdk-wizard.rb
@@ -4,7 +4,7 @@
 class InmobiSdkWizard < Formula
   desc "InMobi SDK integration Wizard"
   homepage "https://github.com/manoj897/android-sdk-integration-wizard"
-  url "https://github.com/manoj897/android-sdk-integration-wizard/archive/refs/tags/v1.0.0.tar.gz"
+  url "https://github.com/manoj897/android-sdk-integration-wizard/archive/refs/tags/v1.0.0.zip"
   sha256 "81978ddd587b0d7a9c45f260451e8f73bfe9e578d3dd3020d82b4c96967d5d8f"
   license ""
 
@@ -12,7 +12,7 @@ class InmobiSdkWizard < Formula
 
   def install
         libexec.install Dir['*']
-        bin.write_jar_script libexec/'android-sdk-inmobisdkwizard.jar' "inmobiwizard"
+        bin.write_jar_script libexec/"android-sdk-inmobisdkwizard.jar" "inmobiwizard"
   end
 
   test do
