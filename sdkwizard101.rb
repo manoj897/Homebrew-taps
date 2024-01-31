9a38c00db5e7e9407622804dd1e1de8c59c7c530
manoj.krishnan <manoj.krishnan@inmobi.com>
2024-01-31 18:18:44 +0530

    add new tap

---
+++ sdkwizard101.rb
@@ -0,0 +1,20 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Sdkwizard101 < Formula
+  desc "SDK WIZARD"
+  homepage "https://github.com/manoj897/SdkIntegrationWizard"
+  url "https://github.com/manoj897/SdkIntegrationWizard/archive/refs/tags/1.0.1.tar.gz"
+  sha256 "10cc1c0afa898338f4684c562dee1c4b28d362b157a05bb8521d38280da57fbf"
+  license ""
+
+  depends_on "openjdk@11"
+
+  def install
+        libexec.install "wizard/output/wizard.jar"
+        bin.write_jar_script libexec/"wizard.jar", "sdkwizard", java_version: "11"
+  end
+
+  test do
+  end
+end
