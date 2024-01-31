9e5885f9c4b6451693c32ede46b663419e56edad
manoj.krishnan <manoj.krishnan@inmobi.com>
2024-01-31 18:15:39 +0530

    update version

--- sdkwizard.rb
+++ sdkwizard.rb
@@ -4,7 +4,7 @@
 class Sdkwizard < Formula
   desc "SDK WIZARD"
   homepage "https://github.com/manoj897/SdkIntegrationWizard"
-  url "https://github.com/manoj897/SdkIntegrationWizard/archive/refs/tags/1.0.0.tar.gz"
+  url "https://github.com/manoj897/SdkIntegrationWizard/archive/refs/tags/1.0.1.tar.gz"
   sha256 "055ac9a0c6f3ea9580958d16f56642e5309accbd83c780a2f5e00e76f1a0889c"
   license ""
 
