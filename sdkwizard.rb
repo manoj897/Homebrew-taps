5e0bdfabf07d40db2cee24f78c5ddde4ce655d01
manoj.krishnan <manoj.krishnan@inmobi.com>
2024-01-31 18:08:38 +0530

    update homepage

--- sdkwizard.rb
+++ sdkwizard.rb
@@ -2,8 +2,8 @@
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Sdkwizard < Formula
-  desc ""
-  homepage ""
+  desc "SDK WIZARD"
+  homepage "https://github.com/manoj897/SdkIntegrationWizard"
   url "https://github.com/manoj897/SdkIntegrationWizard/archive/refs/tags/1.0.0.tar.gz"
   sha256 "055ac9a0c6f3ea9580958d16f56642e5309accbd83c780a2f5e00e76f1a0889c"
   license ""
